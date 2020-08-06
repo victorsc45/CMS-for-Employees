419a9651fff783349a7bc43fc0c1da0c3d818e44
victorsc45 <victorlitzau@gmail.com>
2020-08-06 13:01:28 -0400

    adding packages needed for project

---
+++ cmsEmpSchema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+);
+ 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+);
