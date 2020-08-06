9baa9a303b0e9e85420c6bb8bbac7bdd37711ce3
victorsc45 <victorlitzau@gmail.com>
2020-08-06 16:11:00 -0400

    creating first function for first view

---
+++ employeeCMS.js
@@ -0,0 +1,143 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const console_table = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // port local is 3306
+    port: 3306,
+
+    user: 'root',
+
+    password: 'iC4]mY5*pZ4<uD7&rP8(',
+    database: 'employees'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    startEmpCMS();
+});
+
+function startEmpCMS() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                'View, add, remove, and update Employees?',
+                'View, add, and remove Roles?',
+                'View, add, and remove, Departments?',
+                'View Departments Labor Budgets',
+
+            ]
+        }).then(function (answer) {
+            switch (answer.action) {
+                case 'View, add, remove, and update Employees?':
+                    getEmpData();
+                    break;
+                case 'View, add, and remove Roles?':
+                    getRole();
+                    break;
+
+                case 'View, add, and remove, Departments?':
+                    getDept();
+                    break;
+                case 'View Departments Labor Budgets':
+                    getBudget();
+                    break;
+            }
+        });
+}
+
+function getEmpData() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                'View All Employees?',
+                'Add Employee?',
+                'Update an Employee?',
+                'Remove an Employee?',
+
+            ]
+        }).then(function (answer) {
+            switch (answer) {
+                case 'View All Employees?':
+                    let query = 'SELECT employee.first_name, employee.last_name, department.name, role.titl ';
+                    query += 'FROM employee INNER JOIN role ON (employee.id  = role.id AND department.id';
+                    query += 'WHERE (employee.id = ? AND role.id = ?) ORDER BY department.name';
+                    connection.query(query, [employee.first_name, employee.last_name, department.name, role.title], function (err, res) {
+                        console.table(res.length + 'All the employees at this time!');
+                        for (let i = 0; i < res.length; i++) {
+                            console.table([{
+                                first: i,
+                                last: i,
+                                department: i,
+                                title: i,
+
+                            }
+                            ])
+
+                        }
+
+                    })
+            }
+
+
+        })
+}
+
+
+
+//         .then(function (answer) {
+//     switch (answer.action) {
+//         case "View all Employees?":
+//             viewEmps();
+//             break;
+
+//         case "View all Employess by Department?":
+//             EmpByDept();
+//             break;
+
+//         case "View all Employees by Manager?":
+//             EmpByMngr();
+//             break;
+//         case "Add an Employee?":
+//             addEmp();
+//             break;
+//         case "Remove an Employee?":
+//             deleteEmp();
+//             break;
+//         case "Update Employee Role?":
+//             updateEmpRole();
+//             break;
+//         case "Update Employee Manager?":
+//             updateEmpMngr();
+//             break;
+//         case "View All Roles?":
+//             viewRoles();
+//             break;
+//         case "Add a Role?":
+//             addRole();
+//             break;
+//         case "Remove a Role?":
+//             deleteRole();
+//             break;
+//         case "View All Departments?":
+//             viewDepts();
+//             break;
+//         case "View Department's Labor Budget":
+//             viewBudget();
+//             break;
+//         case "Add a Department?":
+//             deleteRole();
+//             break;
+//         case "Remove a Department?":
+//             deleteDept();
+//     }
+// });
+
+// }
