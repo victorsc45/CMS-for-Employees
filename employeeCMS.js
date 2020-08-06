82e6be00788c6ed8915629600490c6acb97f0554
victorsc45 <victorlitzau@gmail.com>
2020-08-06 18:04:02 -0400

    building out the functions for leveraging mysql calls

--- employeeCMS.js
+++ employeeCMS.js
@@ -53,91 +53,100 @@ function startEmpCMS() {
 function getEmpData() {
     inquirer
         .prompt({
-            name: "action",
+            name: "empData",
             type: "rawlist",
             message: "What would you like to do?",
             choices: [
                 'View All Employees?',
-                'Add Employee?',
+                'Add an Employee?',
+                'View all Employess by Department?',
+                'View all Employees by Manager?',
                 'Update an Employee?',
                 'Remove an Employee?',
 
             ]
         }).then(function (answer) {
-            switch (answer) {
-                case 'View All Employees?':
-                    let query = 'SELECT employee.first_name, employee.last_name, department.name, role.titl ';
-                    query += 'FROM employee INNER JOIN role ON (employee.id  = role.id AND department.id';
-                    query += 'WHERE (employee.id = ? AND role.id = ?) ORDER BY department.name';
-                    connection.query(query, [employee.first_name, employee.last_name, department.name, role.title], function (err, res) {
-                        console.table(res.length + 'All the employees at this time!');
-                        for (let i = 0; i < res.length; i++) {
-                            console.table([{
-                                first: i,
-                                last: i,
-                                department: i,
-                                title: i,
+            switch (answer.empData) {
+                case "View all Employees?":
+                    viewEmps();
+                    break;
 
-                            }
-                            ])
+                case "Add an Employee?":
+                    addEmp();
+                    break;
 
-                        }
+                case "View all Employess by Department?":
+                    EmpByDept();
+                    break;
 
-                    })
+                case "View all Employees by Manager?":
+                    EmpByMngr();
+                    break;
+                case "Update Employee?":
+                    updateEmp();
+                    break;
+
+                case "Remove an Employee?":
+                    deleteEmp();
+                    break;
             }
+        });
+}
 
+function getRole() {
+    inquirer
+        .prompt({
+            name: "roleInfo",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                'View All Roles?',
+                'Add a Role?',
+                'Remove a Role?',
 
+            ]
+        }).then(function (answer) {
+            switch (answer.roleInfo) {
+                case "View All Roles?":
+                    viewRoles();
+                    break;
+                case "Add a Role?":
+                    addRole();
+                    break;
+                case "Remove a Role?":
+                    deleteRole();
+                    break;
+            }
         })
 }
 
 
+function getDept() {
+    inquirer
+        .prompt({
+            name: "deptInfo",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                'View All Departments?',
+                'Add a Department?',
+                'Remove a Department?',
+                'View Departments Labor Budgets',
 
-//         .then(function (answer) {
-//     switch (answer.action) {
-//         case "View all Employees?":
-//             viewEmps();
-//             break;
-
-//         case "View all Employess by Department?":
-//             EmpByDept();
-//             break;
-
-//         case "View all Employees by Manager?":
-//             EmpByMngr();
-//             break;
-//         case "Add an Employee?":
-//             addEmp();
-//             break;
-//         case "Remove an Employee?":
-//             deleteEmp();
-//             break;
-//         case "Update Employee Role?":
-//             updateEmpRole();
-//             break;
-//         case "Update Employee Manager?":
-//             updateEmpMngr();
-//             break;
-//         case "View All Roles?":
-//             viewRoles();
-//             break;
-//         case "Add a Role?":
-//             addRole();
-//             break;
-//         case "Remove a Role?":
-//             deleteRole();
-//             break;
-//         case "View All Departments?":
-//             viewDepts();
-//             break;
-//         case "View Department's Labor Budget":
-//             viewBudget();
-//             break;
-//         case "Add a Department?":
-//             deleteRole();
-//             break;
-//         case "Remove a Department?":
-//             deleteDept();
-//     }
-// });
-
-// }
+            ]
+        }).then(function (answer) {
+            switch (answer.deptInfo) {
+                case "View All Departments?":
+                    viewDepts();
+                    break;
+                case "Add a Department?":
+                    addDept();
+                    break;
+                case "Remove a Department?":
+                    deleteDept();
+                case "View Department's Labor Budget":
+                    viewBudget();
+                    break;
+            }
+        })
+}
